0adf892e432e0ef752a89a55a2894bb9e321cd54
mictlangelo10 <miguehdzss29@gmail.com>
2024-06-24 03:07:43 -0600

    Joder más jale

--- frontend/src/app/models/mantenimiento.interface.ts
+++ frontend/src/app/models/mantenimiento.interface.ts
@@ -1,15 +1,15 @@
 export interface Mantenimiento {
-    id?: number;
-    nombre?: string;
-    descripcion?: string;
-    direccion?: string;
-    fechaRegistro?: Date;
-    aceptado?: boolean;
-    costo?: number;
-    estadoPago?: string;
-    finalizado?: boolean;
-    fechaFin?: Date;
-    usuarioFk?: any;
-    areaFk?: any;
-    empleadoFk?: any;
+  id?: number;
+  nombre?: string;
+  descripcion?: string;
+  direccion?: string;
+  fechaRegistro?: Date;
+  aceptado?: boolean;
+  costo?: number;
+  estadoPago?: string;
+  finalizado?: boolean;
+  fechaFin?: Date;
+  usuarioFk?: any;
+  areaFk?: any;
+  empleadoFk?: any;
 }
